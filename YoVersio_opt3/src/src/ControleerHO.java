94f8e5ab684e1d3c6a19b204a9e88016fe4be80a
bomani12 <81241894+bomani12@users.noreply.github.com>
2022-06-28 20:34:45 +0200

    Delete YoVersio_opt3/src/src directory

--- YoVersio_opt3/src/src/ControleerHO.java
+++
@@ -1,7 +0,0 @@
-public class ControleerHO {
-
-    public boolean CheckHoGeschikt(Huurder huurder, Voertuig voertuig){
-
-    return huurder.getSchadeHistorie() == false && huurder.getTypeRijbewijs().equals(voertuig.getRijbewijsType());
-    }
-}
--- YoVersio_opt3/src/src/VerzekeringPlus.java
+++
@@ -1,9 +0,0 @@
-public class VerzekeringPlus extends Accessoires {
-    public VerzekeringPlus(int keuze){
-        super(keuze, "Verzekering Plus",30.00);
-    }
-    public Double getPrijsLine() {
-        System.out.println("Wilt u gaat u naar het buitenland? (J/N)");
-        return 0.0;
-    }
-}
